82eaa9904fb10b6240aa7635bb631a1896c1c991
muhamed <201834016740@univ-tebessa.dz>
2024-01-02 18:15:11 +0100

    s2

--- lib/input.dart
+++ lib/input.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'cardContent.dart';
+import 'containerCard.dart';
+
+const activeColor = Color(0xFF1D1F33);
+const inactiveColor = Color(0xff111328);
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,6 +13,27 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleColor = inactiveColor;
+  Color femaleColor = inactiveColor;
+
+  void updateColor(int gender) {
+    if (gender == 1) {
+      if (maleColor == inactiveColor) {
+        maleColor = activeColor;
+        femaleColor = inactiveColor;
+      } else {
+        maleColor = inactiveColor;
+      }
+    } else {
+      if (femaleColor == inactiveColor) {
+        femaleColor = activeColor;
+        maleColor = inactiveColor;
+      } else {
+        femaleColor = inactiveColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,47 +48,68 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: containerCard(),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateColor(1);
+                        });
+                      },
+                      child: containerCard(
+                        colour1: maleColor,
+                        cardWidget: CardContent(
+                          awesome: FontAwesomeIcons.mars,
+                          textString: 'MALE',
+                        ),
+                      ),
+                    ),
                   ),
                   Expanded(
-                    child: containerCard(),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateColor(2);
+                        });
+                      },
+                      child: containerCard(
+                        colour1: femaleColor,
+                        cardWidget: CardContent(
+                          awesome: FontAwesomeIcons.venus,
+                          textString: 'FEMALE',
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: containerCard(),
+              child: containerCard(
+                colour1: Color(0xFF1D1F33),
+              ),
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: containerCard(),
+                    child: containerCard(
+                      colour1: Color(0xFF1D1F33),
+                    ),
                   ),
                   Expanded(
-                    child: containerCard(),
+                    child: containerCard(
+                      colour1: Color(0xFF1D1F33),
+                    ),
                   ),
                 ],
               ),
             ),
+            Container(
+              color: Color(0xffeb1555),
+              margin: EdgeInsets.only(top: 10),
+              height: 80,
+              width: double.infinity,
+            )
           ],
         ));
-  }
-}
-
-class containerCard extends StatelessWidget {
-  const containerCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: Color(0xFF1D1F33),
-        borderRadius: BorderRadius.circular(10),
-      ),
-    );
   }
 }
